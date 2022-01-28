b96f27d73047cca3382b8107b208126cb1e6ac91
Gereon Kremer <gkremer@cs.stanford.edu>
2022-01-28 13:26:23 -0800

    Use version string

--- genversions.py
+++ genversions.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import copy
 import glob
 import jinja2
-import os
+import re
 
 tpl_str = """
 <div id="cvc5-versions" class="rst-versions shift-up" data-toggle="rst-versions" role="note" aria-label="versions">
@@ -44,7 +44,7 @@ def put_versions_in_file(filename, newblock):
     nav = doc.find('nav', class_='wy-nav-side')
     if not nav:
         return
-    
+
     # find relative path to root dir
     urlroot = doc.find('script', id='documentation_options')
     if not urlroot:
@@ -52,7 +52,7 @@ def put_versions_in_file(filename, newblock):
     urlroot = f'{urlroot["data-url_root"]}..'
     for a in newblock.find_all('a'):
         a['href'] = a['href'].replace('%URLROOT%', urlroot)
-    
+
     cur = nav.find(id='cvc5-versions')
     if cur:
         cur.replace_with(newblock)
@@ -80,7 +80,12 @@ for version in versions:
     for file in list_files(f'docs-{version}'):
         put_versions_in_file(file, copy.copy(newvers))
 
-stat_versions = map(lambda v: (os.stat(v).st_mtime, v), versions)
-latest_version = sorted(stat_versions)[-1][1]
+# map "cvc5-x.y.z" to (x, y, z, "cvc5-x.y.z")
+stat_versions = map(
+    lambda v:
+    (*map(int,
+          re.match('cvc5-([0-9]+).([0-9]+).([0-9]+)', v).groups()), v),
+    versions)
+latest_version = sorted(stat_versions)[-1][-1]
 newindex = tpl_redirect.render(release=latest_version)
 open("index.html", 'w').write(newindex)
